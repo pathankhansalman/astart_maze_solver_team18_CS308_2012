f5cb0853c27dc819ccc261ca9320072763ff6577
Salman Khan <pathankhan.salman@gmail.com>
2012-04-12 14:42:31 +0530

    Fourth commit

--- tremaux.c
+++ tremaux.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <stdlib>
 #include <math.h>
 #define maze_length 7
 #define maze_width 7
 
+// Declaration of variables
 int is_at_end;
 int path[maze_length][maze_breadth][maze_length][maze_breadth];
+int parent_of[maze_length][maze_breadth];
 
+// Initialization of variables
 void init(){
 	int i,j,k,l;
 	
@@ -24,163 +26,214 @@ void init(){
 	}
 }
 
-
-void trace(int arg1,int arg2,char arg3) {
-	//get current vertices
-	int current_X, current_Y;
-	current_X = arg1;
-	current_y = arg2;
-	int i,j,k,l;
-	for(i=0; i<maze_length;i++) {
-		for(j=0;j<maze_breadth;j++) {
+// Moves to given destination from given source point
+void moveto(int arg1,int arg2,int arg3,int arg4){ 
+	
+	if(arg1 == arg3){
+	
+		if(arg4>arg2){
+			//move forward;
 		}
-	}		
+		
+		else{
+			//turn back;
+			//move forward;
+		}
+	}
+	
+	else if(arg2 == arg4){
+	
+		if(arg3>arg1){
+			//turn left;
+			//move forward;
+		}
+		
+		else{
+			//turn right;
+			//move forward;
+		}
+	}
+	
 }
 
-void tremaux_solving(int arg1,int arg2,char arg3){
+// Checks whether the node has paths to its children or not
+int no_out_path_from(int arg1,int arg2){
+	int child_x,child_y;
+	int flag = 0;
+	int turned_back = 0;
+	
+	child_x = arg1 + 1;
+	child_y = arg2;
+	
+	if(child_x < maze_length && parent_of[arg1][arg2]!=10*child_x + child_y){
+		// turn left
+		// move forward
+		if(!turned_back) flag++;
+	}
+	
+	child_x = arg1 - 1;
+	child_y = arg2;
+	
+	if(child_x > 0 && parent_of[arg1][arg2]!=10*child_x + child_y){
+		// turn right
+		// move forward
+		if(!turned_back) flag++;
+	}
+	
+	child_x = arg1;
+	child_y = arg2 + 1;
+	
+	if(child_y < maze_width && parent_of[arg1][arg2]!=10*child_x + child_y){
+		// move forward
+		if(!turned_back) flag++;
+	}
+	
+	child_x = arg1;
+	child_y = arg2 - 1;
+	
+	if(child_y > 0 && parent_of[arg1][arg2]!=10*child_x + child_y){
+		// turn back
+		// move forward
+		if(!turned_back) flag++;
+	}
+	
+	if(flag>0) return 1;
+	else return 0;
+}
+
+// Checks whether the children have outward paths or not
+int no_child_with_outpath(int arg1,int arg2){
+	int child_x,child_y;
+	int flag = 0;
+	
+	child_x = arg1 + 1;
+	child_y = arg2;
+	
+	if(child_x < maze_length && parent_of[arg1][arg2]!=10*child_x + child_y){
+		// turn left
+		// move forward
+		if(path[arg1][arg2][child_x][child_y]!=2) flag++;
+	}
+	
+	child_x = arg1 - 1;
+	child_y = arg2;
+	
+	if(child_x > 0 && parent_of[arg1][arg2]!=10*child_x + child_y){
+		// turn right
+		// move forward
+		if(path[arg1][arg2][child_x][child_y]!=2) flag++;
+	}
+	
+	child_x = arg1;
+	child_y = arg2 + 1;
+	
+	if(child_y < maze_width && parent_of[arg1][arg2]!=10*child_x + child_y){
+		// move forward
+		if(path[arg1][arg2][child_x][child_y]!=2) flag++;
+	}
+	
+	child_x = arg1;
+	child_y = arg2 - 1;
+	
+	if(child_y > 0 && parent_of[arg1][arg2]!=10*child_x + child_y){
+		// turn back
+		// move forward
+		if(path[arg1][arg2][child_x][child_y]!=2) flag++;
+	}
+	
+	if(flag>0) return 1;
+	else return 0;
+}
+
+// Goto the parent of the given node
+void goto_parent_of(int arg1,int arg2){
 	int current_x,current_y;
-	int ch;
-	char direction;
+	int parent_x,parent_y,parent;
+	
 	current_x = arg1;
 	current_y = arg2;
+	parent = parent_of[current_x][current_y];
+	parent_x = parent/10;
+	parent_y = parent%10;
+	on_optimal_path[current_x][current_y] = 0;
+	
+	moveto(current_x,current_y,parent_x,parent_y);
+}
+
+// Go to one of the children at random
+void goto_child_of(int arg1,int arg2){// Return an integer in this function to depict which child is the bot going to
+	int current_x=arg1,current_y=arg2;
+	int child_x,child_y,no_of_paths;
+	int flag = 0,flagl = 0,flagr = 0,flags = 0,flagb = 0;
+	int random_path_number;
+	
+	child_x = arg1 + 1;
+	child_y = arg2;
+	
+	if(child_x < maze_length && parent_of[arg1][arg2]!=10*child_x + child_y && path[arg1][arg2][child_x][child_y]!=2){
+		// turn left
+		// move forward
+		flag++;
+		flagl++;
+	}
+	
+	child_x = arg1 - 1;
+	child_y = arg2;
+	
+	if(child_x > 0 && parent_of[arg1][arg2]!=10*child_x + child_y && path[arg1][arg2][child_x][child_y]!=2){
+		// turn right
+		// move forward
+		flag++;
+		flagr++;
+	}
+	
+	child_x = arg1;
+	child_y = arg2 + 1;
+	
+	if(child_y < maze_width && parent_of[arg1][arg2]!=10*child_x + child_y && path[arg1][arg2][child_x][child_y]!=2){
+		// move forward
+		flag++;
+		flags++;
+	}
+	
+	child_x = arg1;
+	child_y = arg2 - 1;
+	
+	if(child_y > 0 && parent_of[arg1][arg2]!=10*child_x + child_y && path[arg1][arg2][child_x][child_y]==2){
+		// turn back
+		// move forward
+		flag++;
+		flagb++;
+	}
+	
+	no_of_paths = flag;
+	random_path_number = rand(flag);
+}
+
+// Exploring the maze
+void tremaux_exploring(int arg1,int arg2){
+	int current_x=arg1,current_y=arg2;
+	int successor_x,successor_y,parent_x,parent_y;
 	is_at_end = 0;
 	init();
-	//int no_line_ahead = 1;
-	while(!is_at_end){
+	
+	if(!is_at_end){
 		
-		ch = rand(3);
-		if (end) {
-			is_at_end = 1;
-			//keep the bot at the start again
-			//calling function to trace
-			trace();
-			break;
+		if(no_out_path_from(current_x,current_y) || no_child_with_outpath(current_x,current_y)){
+			goto_parent_of(current_x,current_y);
+			parent_x = parent_of[current_x][current_y]/10;
+			parent_y = parent_of[current_x][current_y]%10;
+			path[parent_x][parent_y][current_x][current_y]++;
+			tremaux_exploring(parent_x,parent_y);
 		}
 		
-		if (no_line_ahead() == 0) {
-			if (if_intersection == 0  && if_half_intersection() == 0) { 
-				//turn around;
-				//go back to parent;
-				if (direction == 'n') {
-					path[current_x][current_y][current_x[current_y+1]++;
-				}
-				
-				if (direction == 's') {
-					path[current_x][current_y][current_x][current_y-1]++;
-				}
-				
-				if (direction == 'e') {
-					path[current_x][current_y][current_x+1][current_y]++;
-				}
-				
-				if (direction == 'w') {
-					path[current_x][current_y][current_x-1][current_y]++;
-				}
-				
-			}
-			
-			else if (if_intersection == 1) {
-				if (ch==2) {
-					ch =rand(2);
-				}
-			}
-			
-			else if (if_half_intersection == -1) {
-				if (ch==1 || ch==2) {
-					ch==0;
-				}
-			}
-			
-			else if (if_half_intersection == 1) {
-				if (ch==0 || ch==2) {
-					ch==1;
-				}
-			}	
+		else{
+			goto_child_of(current_x,current_y);
+			successor_x = successor_value(current_x,current_y)/10;
+			successor_y = successor_value(current_x,current_y)%10;
+			path[current_x][current_y][successor_x][successor_y]++;
+			tremaux_exploring(successor_x,successor_y);
 		}
 		
-		
-		
-		if (if_intersection() == 0 && if_half_intersection() == 0) {
-			if (direction == 'n') {
-				path[current_x][current_y][current_x[current_y+1]++;
-			}
-			
-			if (direction == 's') {
-				path[current_x][current_y][current_x][current_y-1]++;
-			}
-			
-			if (direction == 'e') {
-				path[current_x][current_y][current_x+1][current_y]++;
-			}
-			
-			if (direction == 'w') {
-				path[current_x][current_y][current_x-1][current_y]++;
-			}
-		}
-		
-		else if (ch == 0 && (if_intersection() == 1 || if_half_intersection() == 1)) {
-			turn_bot('l');
-			//call black_line to travel just 1 unit;
-			
-			if (direction == 'n') {
-				path[current_x][current_y][current_x-1][current_y]++;
-			}
-			
-			if (direction == 's') {
-				path[current_x][current_y][current_x+1][current_y]++;
-			}
-			
-			if (direction == 'e') {
-				path[current_x][current_y][current_x][current_y+1]++;
-			}
-			
-			if (direction == 'w') {
-				path[current_x][current_y][current_x][current_y-1]++;
-			}
-			
-		}
-		
-		else if (ch == 1 && (if_intersection() == 1 || if_half_intersection() == 1)) {
-			turn_bot('r');
-			//call black_line to travel just 1 unit;
-			
-			if (direction == 'n') {
-				path[current_x][current_y][current_x+1][current_y]++;
-			}
-			
-			if (direction == 's') {
-				path[current_x][current_y][current_x-1][current_y]++;
-			}
-			
-			if (direction == 'e') {
-				path[current_x][current_y][current_x][current_y-1]++;
-			}
-			
-			if (direction == 'w') {
-				path[current_x][current_y][current_x][current_y+1]++;
-			}
-		
-		}
-		
-		else if (ch == 2 && if_intersection() == 1 ) {
-			//call black_line to go straight one unit;
-			
-			if (direction == 'n') {
-				path[current_x][current_y][current_x[current_y+1]++;
-			}
-			
-			if (direction == 's') {
-				path[current_x][current_y][current_x][current_y-1]++;
-			}
-			
-			if (direction == 'e') {
-				path[current_x][current_y][current_x+1][current_y]++;
-			}
-			
-			if (direction == 'w') {
-				path[current_x][current_y][current_x-1][current_y]++;
-			}
-		
-		}
 	}
 }
